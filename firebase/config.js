b39f971e57fe55974016f851f9378d700c3091ae
IAbrahanArley <abrahanarley96@gmail.com>
2024-06-05 11:38:27 -0300

    first commit with firebase config

---
+++ firebase/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOY9408pxlX99rHWorhPo1POGf9ZUu2VM",
+  authDomain: "miniblog-1c942.firebaseapp.com",
+  projectId: "miniblog-1c942",
+  storageBucket: "miniblog-1c942.appspot.com",
+  messagingSenderId: "966611825823",
+  appId: "1:966611825823:web:b0799f6dfeb7fde5d3deaa"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
